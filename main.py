a3ef10238a824039a57d1452a83d66c6ee1fb588
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-04-04 14:34:40 -0400

    added AI into the mix

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os
+import sys
+
 import pymupdf4llm
 import tiktoken
-import sys
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+
+load_dotenv()
+
+token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+
+azure_openai_uri        = os.getenv("AZURE_OPENAI_ENDPOINT")
+api_version             = os.getenv("AZURE_OPENAI_VERSION", "2024-05-01-preview")
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=str(azure_openai_uri),
+    azure_ad_token_provider=token_provider,
+)
 
 
 def main():
@@ -14,15 +32,33 @@ def main():
     encoding = tiktoken.encoding_for_model('gpt-4o')
     tokens = num_tokens_from_string(md_text, encoding.name)
     print(f"Number of tokens: {tokens}")
-    # now work with the markdown text, e.g. store as a UTF8-encoded file
-    #import pathlib
-    #pathlib.Path("output.md").write_bytes(md_text.encode())
+    sentimemnt_analysis(md_text)
 
 def num_tokens_from_string(string: str, encoding_name: str) -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
+def sentimemnt_analysis(document: str):
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You will do sentiment analysis on the document provided. They are clippings of news articles that pertain to the SSC (Shared Services Canada) media team.",
+            },
+            {
+                "role": "user",
+                "content": document,
+            }
+        ],
+        max_tokens=4096,
+        temperature=1.0,
+        top_p=1.0,
+        model="gpt-4o",
+    )
+
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
