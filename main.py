1114c93361051ab87747570f3fc464dcc11438a9
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-04-04 14:38:06 -0400

    wip: ran ruff

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from openai import AzureOpenAI
 
 load_dotenv()
 
-token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+token_provider = get_bearer_token_provider(
+    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+)
 
-azure_openai_uri        = os.getenv("AZURE_OPENAI_ENDPOINT")
-api_version             = os.getenv("AZURE_OPENAI_VERSION", "2024-05-01-preview")
+azure_openai_uri = os.getenv("AZURE_OPENAI_ENDPOINT")
+api_version = os.getenv("AZURE_OPENAI_VERSION", "2024-05-01-preview")
 
 client = AzureOpenAI(
     api_version=api_version,
@@ -26,19 +28,21 @@ def main():
         print("Usage: python main.py <input_file>")
         return
 
-    input_file =  "./documents/" + sys.argv[1]
+    input_file = "./documents/" + sys.argv[1]
     print(f"Processing file: {input_file}")
     md_text = pymupdf4llm.to_markdown(input_file, write_images=False)
-    encoding = tiktoken.encoding_for_model('gpt-4o')
+    encoding = tiktoken.encoding_for_model("gpt-4o")
     tokens = num_tokens_from_string(md_text, encoding.name)
     print(f"Number of tokens: {tokens}")
     sentimemnt_analysis(md_text)
+
 
 def num_tokens_from_string(string: str, encoding_name: str) -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
 
 def sentimemnt_analysis(document: str):
     response = client.chat.completions.create(
@@ -50,7 +54,7 @@ def sentimemnt_analysis(document: str):
             {
                 "role": "user",
                 "content": document,
-            }
+            },
         ],
         max_tokens=4096,
         temperature=1.0,
@@ -59,6 +63,7 @@ def sentimemnt_analysis(document: str):
     )
 
     print(response.choices[0].message.content)
+
 
 if __name__ == "__main__":
     main()
